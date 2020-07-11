7d045dcdf7ff42d4874aece8eadf7c3b1fe4916e
Uday Sambyal <sambyal.uday9@gmail.com>
2020-07-11 12:53:03 +0530

    Update get_testcase2.py

--- TestCases2/get_testcase2.py
+++ TestCases2/get_testcase2.py
@@ -5,7 +5,7 @@ response = requests.get(base_url)
 
 json_response = json.loads(response.text)
 pages = jsonpath.jsonpath(json_response,'total_pages')
-assert pages[0] == 3
+assert pages[0] == 2,"Page should be 2"
 
 # Checking for GET Assertion error
 assert response.status_code == 200
